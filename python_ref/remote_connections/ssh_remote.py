ebd23f301262659e9f2fac1ca43e38dc9d0eba06
pasu-t <pasupathi.thumburu@gmail.com>
2019-04-08 22:30:46 +0530

    new files

--- python_ref/remote_connections/ssh_remote.py
+++ python_ref/remote_connections/ssh_remote.py
@@ -1,2 +1,13 @@
-import pxssh
-help(pxssh)
+import paramiko
+ip='test.rebex.net'
+port=22
+username = 'demo' 
+password = 'password'
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(ip,port,username,password)
+stdin,stdout,stderr=ssh.exec_command('ls')
+outlines=stdout.readlines()
+resp=''.join(outlines)
+print(resp)
--- python_ref/remote_connections/windows_remote.py
+++ python_ref/remote_connections/windows_remote.py
@@ -1,7 +1,7 @@
-ip = '172.20.12.151'
-user = ''
-pswd = ''
 from socket import *
+ip = '192.168.0.101'
+user = 'Public'
+pswd = ''
 try:
 	print('establishing connection')
 	connection = wmi.WMI(ip, user = user, password = pswd)

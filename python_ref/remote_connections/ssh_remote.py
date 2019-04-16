c114b21d6659dccacfdf9a30535f87711b63ef0d
pthumbur <pasupathi.thumbur@adtran.com>
2019-04-16 17:18:36 +0530

    modified scripts

--- python_ref/remote_connections/ssh_remote.py
+++ python_ref/remote_connections/ssh_remote.py
@@ -1,13 +1,19 @@
-import paramiko
-ip='test.rebex.net'
-port=22
-username = 'demo' 
-password = 'password'
+# import paramiko
+# ip='10.49.20.152'
+# port=22
+# username = 'root' 
+# password = 'adtran1234'
 
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(ip,port,username,password)
-stdin,stdout,stderr=ssh.exec_command('ls')
-outlines=stdout.readlines()
-resp=''.join(outlines)
-print(resp)
+# ssh = paramiko.SSHClient()
+# ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+# ssh.connect(ip,port,username,password)
+# stdin,stdout,stderr=ssh.exec_command('ls')
+# outlines=stdout.readlines()
+# resp=''.join(outlines)
+# print(resp)
+import subprocess
+import sys
+from fabric import Connection
+print(help(Connection))
+result = Connection('10.49.20.156').run('uname -s', hide=True)
+print(result)

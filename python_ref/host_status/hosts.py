562a2e279ff11f6d0eb42da23687daa4711d8911
pthumbur <pasupathi.thumbur@adtran.com>
2019-04-26 16:09:36 +0530

    finally

--- python_ref/host_status/hosts.py
+++ python_ref/host_status/hosts.py
@@ -10,16 +10,16 @@ import smtplib
 from email.message import EmailMessage
 
 #Gobal variables
-list_contacts = ['pasupathi.thumburu@gmail.com', 'pasupathi.thumbur@adtran.com']
+list_contacts = ['pasupathi.thumburu@gmail.com', 'pasupathi.thumbur@adtran.com',]
 list_files = ['hosts_status.csv']
-
 ip_list = []
 hosts_name = []
 ping_status = []
 ping_health = []
 ip_list_windows = []
 ip_list_linux = []
-remote_conn_status_dict = []
+remote_conn_status = []
+final_remote_conn_status = []
 #path = 'C:\\Users\\Home\\Desktop\\pasi\\python_ref\\csv_operations\\hosts.csv'
 
 with open('hosts.csv', 'r') as hosts_file:
@@ -30,7 +30,7 @@ with open('hosts.csv', 'r') as hosts_file:
 		if line['os'] == 'windows':
 			ip_list_windows.append(line['ip_address'])
 		else:
-			ip_list_linux.append(line['ip_address'])
+			ip_list_linux.append([line['ip_address'],line['user'],line['password']])
 	
 def ping_ip(ip_addr):
 	'''
@@ -40,13 +40,13 @@ def ping_ip(ip_addr):
 	[out, error] = ping.communicate()
 	output = out.decode()
 	if not error:
-		ping_stats = re.search(r'.*Sent = (\d+),.*(\d+),.*(\d+)%' , output)
+		#ping_stats = re.search(r'.*Sent = (\d+),.*(\d+),.*(\d+)%' , output)
 		c_reply = output.lower().count('ttl')
 		c_timeout = output.lower().count('timed out')
 		c_dest = output.lower().count('destination')
-		c_loss = int(ping_stats.group(3))
+		#c_loss = int(ping_stats.group(3))
 		ping_status.append({ip_addr:[c_reply,c_timeout,c_dest]})
-		# print(c_reply, c_timeout, c_dest)
+		#print(ip_addr, c_reply, c_timeout, c_dest)
 	else:
 		print('please check the given input')
 		exit()
@@ -73,14 +73,22 @@ def start_ping():
 def check_ping_health():
 	'''
 	this function gives the percentage of successful pings
+	rearranges in a order and then applies the formula to calculate health
 	'''
-	for each in ping_status:
-		for value in each.values():
-			ping_health.append(int((value[0]/4)*100))
+	ping_status2 = []
+	for i in ip_list:
+		for j in ping_status:
+			try :
+				ping_status2.append(j[i])
+			except KeyError:
+				pass
+	for each in ping_status2:
+		ping_health.append(int((each[0]/4)*100))
 
 def windows_remote_check(ip_addr):
 	'''
 	this function verifies whether remote connection is enabled for the given ip address of windows machine
+	appends the status(element format --> {ip:status}) to remote_conn_status list variable
 	'''
 	rdp_cmd = """If (New-Object System.Net.Sockets.TCPClient -ArgumentList """ + ip_addr + """,3389) { Write-Host 'RDP is open' }
 	If ($? -eq $false) { Write-Host 'Something went wrong' }"""
@@ -88,12 +96,12 @@ def windows_remote_check(ip_addr):
 	[out, error] = rdp_out.communicate()
 	rdp_status = out.decode()
 	if 'RDP is open' in rdp_status:
-		remote_conn_status_dict.append({ip_addr : 'Success'})
+		remote_conn_status.append({ip_addr : 'Success'})
 		#print(rdp_status)
 	else:
 		print('A remote connection attempt failed to ',ip_addr)
 		#print(error.decode())
-		remote_conn_status_dict.append({ip_addr :'Fail'})
+		remote_conn_status.append({ip_addr :'Fail'})
 
 def start_windows_remote_check():
 	'''
@@ -117,7 +125,7 @@ def start_windows_remote_check():
 def ssh_connect(ssh_conn_list):
 	'''
 	ssh_conn_list is the list object contains ip,user,password elements [ipaddresss, user, password]
-	Tries ssh remote connection and appends the status to linux_remote_check list variable
+	Tries ssh remote connection and appends the status(element format --> {ip:status}) to remote_conn_status list variable
 	'''
 	try:
 	    ssh = paramiko.SSHClient()
@@ -125,14 +133,14 @@ def ssh_connect(ssh_conn_list):
 	    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 	    paramiko.util.log_to_file("Exception.log")
 	    ssh.connect(ssh_conn_list[0],username=ssh_conn_list[1],password=ssh_conn_list[2])
-	    print("Connected to", ssh_conn_list[0])
-	    remote_conn_status_dict.append({ssh_conn_list[0] : 'Success'})
+	    #print("Connected to", ssh_conn_list[0])
+	    remote_conn_status.append({ssh_conn_list[0] : 'Success'})
 	except paramiko.AuthenticationException:
-	    print("Failed to connect to" , ssh_conn_list[0] , "due to wrong username/password")
-	    remote_conn_status_dict.append({ssh_conn_list[0] : 'Fail'})
+	    #print("Failed to connect to" , ssh_conn_list[0] , "due to wrong username/password")
+	    remote_conn_status.append({ssh_conn_list[0] : 'Fail'})
 	except Exception as e:
 	    #print(e)
-	    remote_conn_status_dict.append({ssh_conn_list[0] : 'Fail'}) 
+	    remote_conn_status.append({ssh_conn_list[0] : 'Fail'}) 
 	finally:
 		if ssh: 
 			ssh.close()
@@ -155,6 +163,17 @@ def start_linux_remote_check():
 				continue
 			else:
 				raise
+def get_remote_conn_status():
+	'''
+	this function does fetch remote status string from remote_conn_status and sort them as per ip_list 
+	the strings are then appended to final_remote_conn_status
+	'''
+	for i in ip_list:
+		for j in remote_conn_status:
+			try :
+				final_remote_conn_status.append(j[i])
+			except KeyError:
+				pass
 
 def create_csv():
 	'''
@@ -163,7 +182,7 @@ def create_csv():
 	print('started creating csv file for host status !!!')
 	with open('hosts_status.csv', 'w') as new_file:
 		fieldnames = ['ip_address', 'host_name', 'ping_health', 'remote_conn_status']
-		output_lines = tuple(map(lambda a,b,c,d: [a,b,c,d],ip_list, hosts_name, ping_health, remote_conn_status_dict))
+		output_lines = tuple(map(lambda a,b,c,d: [a,b,c,d],ip_list, hosts_name, ping_health, final_remote_conn_status))
 		csv_writer = csv.writer(new_file)
 		csv_writer.writerow(fieldnames)
 		for line in output_lines:
@@ -173,11 +192,10 @@ def send_email(list_files,list_contacts):
 	'''
 	This method will send the mail to list_contacts with attachments of list_files
 	''' 
-	#contacts = ['pasupathi.thumburu@gmail.com']
 	email_address= 'pasupathi.thumburu@gmail.com'
 	email_password = os.environ.get('EMAIL_PASSWORD')
 	msg = EmailMessage()
-	msg.set_content('Files attached...')
+	msg.set_content('Refer files attached...')
 	msg['Subject'] = 'Current host status'
 	msg['From'] = email_address
 	msg['To'] = list_contacts
@@ -195,11 +213,11 @@ def send_email(list_files,list_contacts):
 			print('Please check the email credentials. Seems got error while login or sendind an email')
 		smtp.send_message(msg)
 
-start_ping()
-check_ping_health()
-start_windows_remote_check()
-start_linux_remote_check()
-create_csv()
-send_email(list_files,list_contacts)
-
-
+if __name__ == "__main__":
+	start_ping()
+	check_ping_health()
+	start_windows_remote_check()
+	start_linux_remote_check()
+	get_remote_conn_status()
+	create_csv()
+	send_email(list_files,list_contacts)

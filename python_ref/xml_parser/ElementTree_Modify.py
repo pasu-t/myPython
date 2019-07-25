b4aea0facd440c2fe838128d3e6feb44888ea372
pasu-t <pasupathi.thumburu@gmail.com>
2019-07-25 22:21:33 +0530

    xml parser examples

--- python_ref/xml_parser/ElementTree_Modify.py
+++ python_ref/xml_parser/ElementTree_Modify.py
@@ -2,7 +2,7 @@ import xml.etree.ElementTree as ET
 mytree = ET.parse('sample.xml')
 myroot = mytree.getroot()
 print(dir(ET))
-ET.subElement(myroot[0], 'speciality')
+ET.SubElement(myroot[0], 'speciality')
 for x in myroot.iter('speciality'):
 	b = "South Indian special"
 	x.text = b

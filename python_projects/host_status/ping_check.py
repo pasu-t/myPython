cd32ef45919dd2b2e5617797a97d3df40ad76dff
pasu-t <pasupathi.thumburu@gmail.com>
2019-12-29 18:22:28 +0530

    modify

--- python_projects/host_status/ping_check.py
+++ python_projects/host_status/ping_check.py
@@ -1,11 +1,9 @@
-import os
-def check_ping(host):
-    response = os.system("ping -n 4 " + host)
-    # and then check the response...
-    if response == 0:
-        pingstatus = "Network Active"
-    else:
-        pingstatus = "Network Error"
+from multiping import multi_ping
 
-    return pingstatus
-print(check_ping('192.168.0.100'))
+addrs = ["192.168.0.1", "192.168.0.100", "192.168.0.2", "192.168.0.102", "192.168.0.101"]
+responses, no_responses = multi_ping(addrs, timeout=3, retry=3)
+# Ping the addresses up to 4 times (initial ping + 3 retries), over the
+# course of 2 seconds. This means that for those addresses that do not
+# respond another ping will be sent every 0.5 seconds.
+for addr, rtt in responses.items():
+    print(f"{addr} responded in {rtt} seconds")	

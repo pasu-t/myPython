be27050d415416f7b2e4cd3c6d601948abbba20d
pasu-t <46959852+pasu-t@users.noreply.github.com>
2021-12-08 18:01:59 +0530

    Update jira_status_update.py

--- python_modules/jira/jira_status_update.py
+++ python_modules/jira/jira_status_update.py
@@ -1,7 +1,7 @@
 from jira.client import JIRA
 options = {
     'server': 'https://jira.adtran.com'}
-authed_jira = JIRA(basic_auth=('pthumbur', 'Kajal@143'), options = options)
+authed_jira = JIRA(basic_auth=('pthumbur', ''), options = options)
 
 myissue = authed_jira.issue('AD-135311')
 transitions = authed_jira.transitions(myissue)
